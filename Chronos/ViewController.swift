cf9b8a21c5e87480c935dadc849bf40dcc6d2148
Yanki Insel <yankiinsel@gmail.com>
2018-10-29 18:05:42 +0300

    first commit

---
+++ Chronos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Chronos
+//
+//  Created by Yanki Insel on 28.10.2018.
+//  Copyright © 2018 Yanki Insel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
